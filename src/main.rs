a2456023274a77e30c8bafee5add6ab2b4880a2b
francd <fcamadi@gmail.com>
2023-01-22 20:43:23 +0100

    feat: add render loop in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::error::Error;
-use std::io;
+use std::{io, thread};
+use std::sync::mpsc;
 use std::time::Duration;
 use crossterm::{event, ExecutableCommand, terminal};
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
+use invaders::{frame, render};
 
 fn main() -> Result <(), Box<dyn Error>> {
 
@@ -26,6 +28,24 @@ fn main() -> Result <(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;  // "execute" is an extension provided by crossterm
                                                      // to ommediatelly execute something
     stdout.execute(Hide)?;
+
+    //render loop
+    // it is better to use crossbeam channels, because they are better performant and have more features,
+    // but for this project, mpsc channels are fine (they are built in the standard library).
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move ||  {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
 
     'gameloop: loop {
         //Input
